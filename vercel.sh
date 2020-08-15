1642c3a945450f530264be5d58e0c4f1893fd556
Ignacio Anaya <ignacio.anaya89@gmail.com>
2020-08-15 03:08:21 -0300

    Add global env port

---
+++ vercel.sh
@@ -0,0 +1,2 @@
+# now -e PMA__SPOTIFY__CLIENT_ID=@spotify-client-id -e PMA__SPOTIFY__CLIENT_SECRET=@spotify-client-secret -e PMA__LOGGER__SENTRY__DSN=@sentry-dsn -e PMA__LOGGER__LOGGLY__SUBDOMAIN=@loggly-subdomain -e PMA__LOGGER__LOGGLY__INPUT_TOKEN=@loggly-input-token
+# now alias
